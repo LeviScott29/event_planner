5227717f653b9d11df859eec336e9ba0b6a7690a
levi <angel.of.secrets8@gmail.com>
2024-11-02 15:50:21 -0400

    added lists and start button

--- src/Components/EventPlanner.jsx
+++ src/Components/EventPlanner.jsx
@@ -7,7 +7,46 @@ const EventPlanner = () => {
             <header>
                 <h1>Welcome to Event Planner</h1>
             </header>
-        </div>
+            <section className= "description">
+                <p>
+                Plan and organize your events effortlessly with Event Planner. From
+                    birthdays to corporate meetings, we've got you covered.
+                </p>
+                <button className="get-started-button">
+                  Get Started  
+                </button>
+                </section>  
+            <section className="event_categories">
+                <ul>
+                    <h2>Social Events: </h2>
+                    <li>Birthday</li>
+                    <li>Anniversary</li>
+                    <li>Wedding</li>
+                    <li>Baby</li>
+                    <li>Graduation</li>
+                    <li>Family</li>
+                </ul>
+                <ul>
+                    <h2>Entertainment Events:</h2>
+                    <li>Concerts</li>
+                    <li>Music</li>
+                    <li>Fiilm</li>
+                    <li>Comedy</li>
+                    <li>Art</li>
+                    <li>Cultural</li>
+                </ul>
+                <ul>
+                    <h2>Community Events: </h2>
+                    <li></li>
+                </ul>
+                </section>
+            <section clasName="features">
+                </section>
+            <section className="testimonials" >
+                </section>
+            <section className="contact">
+                </section>     
+         </div>
     );
 };
 
